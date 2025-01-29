342a338ec28ce5c995225b91b4376a5514e78829
SelyaSharma <selyasharma5@gmail.com>
2025-01-29 21:04:04 +0530

    Solution to Count Even and Odd Numbers in an Array

---
+++ Count Even and Odd Numbers in an Array@@676c02002dbc8c3e7a280b71/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a,even=0;
+    int odd=0;
+    scanf("%d",&a);
+    int s[a];
+    for(i=1;i<=a;i++){
+        scanf("%d",s[i]);
+    }
+    for(j=1;j<=s[a];j++){
+        if(a%2==0){
+            even+=1;
+        }
+        else{
+            odd+=1;
+        }
+    }
+printf("%d",even);
+printf("%d",odd)
+    return 0;
+}
